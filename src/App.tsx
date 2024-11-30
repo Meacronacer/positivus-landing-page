b48a396cd94355101e393947ded5975e96b5e36d
Meacronacer <sinnerscrown09@gmail.com>
2024-11-30 16:21:24 +0200

    team adaptive section layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import CaseStudioItem from './components/shared/caseStudioItem';
 import GuideItem from './components/shared/guideItem';
 import Header from './components/shared/header';
 import ServiceItem from './components/shared/serviceItem';
+import TeamItem from './components/shared/teamItem';
 import { Button } from './components/ui/button';
 
 const ourParnetrs = [
@@ -15,7 +16,7 @@ const ourParnetrs = [
 
 const App: React.FC = () => {
   return (
-    <section className="dekstop:pt-[30px] dekstop:px-[40px] mobile:pt-[15px] mobile:px-[20px] mx-auto max-w-[1440px] overflow-x-hidden px-[100px] pb-[200px] pt-[60px]">
+    <section className="mx-auto max-w-[1440px] overflow-x-hidden px-[100px] pb-[200px] pt-[60px] dekstop:px-[40px] dekstop:pt-[30px] mobile:px-[20px] mobile:pt-[15px]">
       <Header />
 
       <main className="mt-[70px] tablet:mt-[40px]">
@@ -44,7 +45,7 @@ const App: React.FC = () => {
               />
             </div>
           </div>
-          <ul className="mobile:mt-[60px] mobile:w-[calc(100%+120px)] mobile:ml-[-60px] mt-[70px] flex items-center justify-between laptop:grid laptop:grid-cols-3 laptop:gap-y-5">
+          <ul className="mt-[70px] flex items-center justify-between laptop:grid laptop:grid-cols-3 laptop:gap-y-5 mobile:ml-[-60px] mobile:mt-[60px] mobile:w-[calc(100%+120px)]">
             <h2 className="hidden">Our partners</h2>
             {ourParnetrs.map((item, index) => (
               <li
@@ -67,7 +68,7 @@ const App: React.FC = () => {
         {/* services */}
         <section className="mt-[130px] laptop:mt-[60px]">
           <div className="flex items-center gap-x-[40px] gap-y-[30px] laptop:flex-col">
-            <h3 className="bg-green flex h-[51px] items-center rounded-[7px] px-[7px] text-[40px] font-medium">
+            <h3 className="flex h-[51px] items-center rounded-[7px] bg-green px-[7px] text-[40px] font-medium">
               Services
             </h3>
             <p className="max-w-[580px] text-[18px] leading-[130%]">
@@ -76,7 +77,7 @@ const App: React.FC = () => {
             </p>
           </div>
 
-          <div className="dekstop:grid-cols-1 mt-[80px] grid grid-cols-2 gap-[40px] laptop:mt-[40px] laptop:gap-[30px]">
+          <div className="mt-[80px] grid grid-cols-2 gap-[40px] dekstop:grid-cols-1 laptop:mt-[40px] laptop:gap-[30px]">
             <ServiceItem
               title1="Search engine"
               title2="optimization"
@@ -124,8 +125,8 @@ const App: React.FC = () => {
         </section>
 
         {/* let's make things happens  */}
-        <section className="bg-grey relative mt-[124px] rounded-[45px] p-[60px] tablet:mt-[70px] tablet:p-[50px]">
-          <div className="mobile:text-left mobile mobile:gap-y-5 flex max-w-[500px] flex-col gap-y-[26px] laptop:max-w-[400px] laptop:text-center tablet:max-w-full tablet:items-center">
+        <section className="relative mt-[124px] rounded-[45px] bg-grey p-[60px] tablet:mt-[70px] tablet:p-[50px]">
+          <div className="mobile flex max-w-[500px] flex-col gap-y-[26px] laptop:max-w-[400px] laptop:text-center tablet:max-w-full tablet:items-center mobile:gap-y-5 mobile:text-left">
             <span className="block text-[30px] font-medium leading-[130%]">
               Let’s make things happen
             </span>
@@ -138,14 +139,14 @@ const App: React.FC = () => {
           <img
             src="thingsHappens.svg"
             alt="thingsHappens"
-            className="dekstop:right-0 absolute right-[135px] top-[-20px] laptop:top-6 laptop:w-[30%] tablet:hidden"
+            className="absolute right-[135px] top-[-20px] dekstop:right-0 laptop:top-6 laptop:w-[30%] tablet:hidden"
           />
         </section>
 
         {/* case studies */}
         <section className="mt-[160px] laptop:mt-[60px]">
           <div className="flex items-center gap-x-[40px] gap-y-[30px] laptop:flex-col">
-            <h4 className="bg-green flex h-[51px] items-center rounded-[7px] px-[7px] text-[40px] font-medium">
+            <h4 className="flex h-[51px] items-center rounded-[7px] bg-green px-[7px] text-[40px] font-medium">
               Case Studies
             </h4>
             <p className="max-w-[580px] leading-[130%]">
@@ -154,16 +155,16 @@ const App: React.FC = () => {
             </p>
           </div>
 
-          <div className="bg-dark dekstop:p-10 dekstop:items-center mt-[80px] flex h-[326px] justify-center rounded-[45px] px-[60px] py-[70px] text-white laptop:mt-[40px] tablet:grid tablet:grid-flow-col tablet:justify-start tablet:gap-x-[20px] tablet:overflow-x-scroll tablet:bg-white tablet:p-0">
+          <div className="mt-[80px] flex h-[326px] justify-center rounded-[45px] bg-dark px-[60px] py-[70px] text-white dekstop:items-center dekstop:p-10 laptop:mt-[40px] tablet:grid tablet:grid-flow-col tablet:justify-start tablet:gap-x-[20px] tablet:overflow-x-scroll tablet:bg-white tablet:p-0">
             <CaseStudioItem text="For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales." />
             <img
               src="line.svg"
-              className="dekstop:px-[32px] px-[64px] tablet:hidden"
+              className="px-[64px] dekstop:px-[32px] tablet:hidden"
             />
             <CaseStudioItem text="For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic." />
             <img
               src="line.svg"
-              className="dekstop:px-[32px] px-[64px] tablet:hidden"
+              className="px-[64px] dekstop:px-[32px] tablet:hidden"
             />
             <CaseStudioItem text="For a national retail chain, we created a social media marketing campaign that increased followers by 25% and generated a 20% increase in online sales." />
           </div>
@@ -172,10 +173,10 @@ const App: React.FC = () => {
         {/* our working process */}
         <section className="mt-[140px] tablet:mt-[60px]">
           <div className="flex items-center gap-x-[40px] gap-y-[30px] laptop:flex-col">
-            <h4 className="bg-green mobile:text-[36px] flex h-[51px] items-center rounded-[7px] px-[7px] text-center text-[40px] font-medium">
+            <h4 className="flex h-[51px] items-center rounded-[7px] bg-green px-[7px] text-center text-[40px] font-medium mobile:text-[36px]">
               Our Working Process
             </h4>
-            <p className="mobile:text-center max-w-[292px] leading-[130%]">
+            <p className="max-w-[292px] leading-[130%] mobile:text-center">
               Step-by-Step Guide to Achieving Your Business Goals
             </p>
           </div>
@@ -192,6 +193,61 @@ const App: React.FC = () => {
               <GuideItem key={index} title={item} index={index} />
             ))}
           </div>
+        </section>
+
+        {/* Team */}
+        <section className="mt-[140px] tablet:mt-[60px]">
+          <div className="flex items-center gap-x-[40px] gap-y-[30px] laptop:flex-col">
+            <h4 className="flex h-[51px] items-center rounded-[7px] bg-green px-[7px] text-[40px] font-medium">
+              Team
+            </h4>
+            <p className="max-w-[473px] leading-[130%] laptop:text-center">
+              Meet the skilled and experienced team behind our successful
+              digital marketing strategies
+            </p>
+          </div>
+
+          <div className="mt-[80px] grid grid-cols-3 gap-[40px] dekstop:grid-cols-2 tablet:mt-[40px] tablet:grid-cols-1">
+            <TeamItem
+              image="picture.png"
+              name="John Smith"
+              position="CEO and Founder"
+              description="10+ years of experience in digital marketing. Expertise in SEO, PPC, and content strategy"
+            />
+            <TeamItem
+              image="team2.png"
+              name="Jane Doe"
+              position="Director of Operations"
+              description="7+ years of experience in project management and team leadership. Strong organizational and communication skills"
+            />
+            <TeamItem
+              image="team3.png"
+              name="Michael Brown"
+              position="Senior SEO Specialist"
+              description="5+ years of experience in SEO and content creation. Proficient in keyword research and on-page optimization"
+            />
+            <TeamItem
+              image="team4.png"
+              name="Emily Johnson"
+              position="PPC Manager"
+              description="3+ years of experience in paid search advertising. Skilled in campaign management and performance analysis"
+            />
+            <TeamItem
+              image="team5.png"
+              name="Brian Williams"
+              position="Social Media Specialist"
+              description="4+ years of experience in social media marketing. Proficient in creating and scheduling content, analyzing metrics, and building engagement"
+            />
+            <TeamItem
+              image="team6.png"
+              name="Sarah Kim"
+              position="Content Creator"
+              description="2+ years of experience in writing and editing Skilled in creating compelling, SEO-optimized content for various industries"
+            />
+          </div>
+          <Button className="float-right mt-[40px] tablet:w-full">
+            See all team
+          </Button>
         </section>
       </main>
     </section>
---
+++ src/components/shared/teamItem.tsx
@@ -0,0 +1,28 @@
+interface props {
+  image: string;
+  name: string;
+  position: string;
+  description: string;
+}
+
+const TeamItem: React.FC<props> = ({ image, name, position, description }) => {
+  return (
+    <div className="rounded-[45px] border-[1px] px-[35px] py-[40px] shadow-[rgba(0,0,15,1)_0px_5px_0px_0px]">
+      <div className="relative flex items-end gap-x-[20px]">
+        <img src={image} alt="profile" loading="lazy" />
+        <div className="flex flex-col">
+          <span className="text-[20px] font-medium">{name}</span>
+          <span className="leading-[100%]">{position}</span>
+        </div>
+        <img
+          src="linkedin.svg"
+          className="absolute right-0 top-0 cursor-pointer duration-300 hover:scale-110"
+        />
+      </div>
+      <hr className="my-[28px]" />
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default TeamItem;
